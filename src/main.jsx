b81f0cbd69e39dfe5e42d2c03df66656ec8fc320
Gustavo1701 <gustavojr.lg.10@gmail.com>
2024-07-21 11:01:26 -0300

    Subindo projeto

---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './Pages/Layout'
+import Home from './Pages/Home'
+import Tecnologias from './Pages/Tecnologias'
+import Contatos from './Pages/Contatos'
+
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="tecnologias" element={<Tecnologias />} />
+          <Route path="contatos" element={<Contatos />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
